241c41b73394f91abf57a6cb3e6fd81ebeb05803
Nipun-Dhananjaya <nipundhananjaya967@gmail.com>
2023-05-24 11:20:05 +0530

    Util Class created

---
+++ Layered_Architecture/src/util/CrudUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+import db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T>T execute(String sql,Object... args) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement = DBConnection.getDbConnection().getConnection().prepareStatement(sql);
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i+1,args[i]);
+        }
+        if (sql.startsWith("SELECT")|| sql.startsWith("select")){
+            return (T) preparedStatement.executeQuery();
+        }else{
+            return (T) (Boolean) (preparedStatement.executeUpdate()>0);
+        }
+    }
+}
